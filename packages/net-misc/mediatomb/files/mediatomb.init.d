6f7ac4d228b07867f52818f7324eaf12b2fff620
Wulf C. Krueger <philantrop@exherbo.org>
2012-05-20 20:45:51 +0200

    mediatomb: Don't install to/use /var/run.

--- packages/net-misc/mediatomb/files/mediatomb.init.d
+++ packages/net-misc/mediatomb/files/mediatomb.init.d
@@ -10,9 +10,9 @@ depend() {
 start() {
     ebegin "Starting MediaTomb"
     start-stop-daemon --start --quiet \
-    --pidfile /var/run/mediatomb.pid --exec /usr/bin/mediatomb -- \
+    --pidfile /run/mediatomb.pid --exec /usr/bin/mediatomb -- \
     -c /etc/mediatomb/config.xml -u mediatomb -g mediatomb \
-    -P /var/run/mediatomb.pid -l /var/run/mediatomb.log -m /var/lib -f mediatomb \
+    -P /run/mediatomb.pid -l /run/mediatomb.log -m /var/lib -f mediatomb \
     -d
     eend $?
 }
